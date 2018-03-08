00007f46f5c06ac5d1616d4cc463adf91e765a8c
abdullahaslan <a.aslana@gmail.com>
2018-03-08 15:50:08 +0300

    Update Promotions.sol
    Give some roles to company

--- contracts/Promotions.sol
+++ contracts/Promotions.sol
@@ -11,28 +11,32 @@ import "zeppelin-solidity/contracts/ownership/Ownable.sol";
 
 contract Promotions is Ownable {
     
+    
+    using SafeMath for uint;
+    
     uint public tokensMinted = 0;
     uint public minimumSupply = 1; //minimum token amount to sale at one transaction
-    uint32 minimumReward = 1;
-    uint32 maximumReward = 1000;
-    uint insertProductWei = 100;
-    uint public constant MAXIMUM_SUPPLY = 2500000 * 10**18;       
+    uint insertBarcodeWei = 1*10**14;
+    uint256 public constant MAXIMUM_SUPPLY = 2500000 * 10**18;       
 
     MintableToken token;
     
-    event InsertCompanyEvent();
-    event InsertProductEvent(address _companyAddress, uint32 _productId);
-    event InsertBarcodeEvent(address _companyAddress,  uint32 _productId, uint32 _barcodeNumber);
+    event AddCompanyEvent();
+     event LoadBalanace(uint256 _balance);
+    event AddProductEvent(address _companyAddress, uint32 _productId);
+    event AddBarcodeEvent(address _companyAddress,  uint32 _productId, uint32 _barcodeNumber);
     event CheckBarcodeEvent(address _checkerAddress,uint32 _productId, uint32 _barcodeNumber);
     event UserWonEvent(address _checkerAddress, uint32 _productId, uint32 _barcodeNumber, uint32 _coinAmount);
   
-    enum Status { Active, Passive }
+    enum Status { Registered, Payed, Stopped }
+    
+    enum AvaliableStatus { Active, Passive }
     
     struct barcode {
         bytes32 barcodeId; 
         uint256 timestamp; 
         uint32 rewardAmountInWei;
-        Status status;
+        AvaliableStatus status;
     }
 
     struct product {
@@ -42,14 +46,15 @@ contract Promotions is Ownable {
         uint256 endDate;             
         uint32 promotionCount;       
         uint256 timestamp;
-        Status status;
+        AvaliableStatus status;
     }
     
     struct company{
         string companyName;
         uint32 balance;
         uint256 timestamp;
-        address companyAddress;
+        string name;
+        string email;
         Status status;
     }
 
@@ -74,50 +79,57 @@ contract Promotions is Ownable {
     
     address public owner;    // address of the token being used for this   
 
+    address LYAccount = 0x4558d71CA1eda15601767dbA2114Ed1c6d150Eb6;
     /// @notice the init function that is run automatically when contract is created
     function Promotions() {
         owner = msg.sender;
         token = new PromotionToken();
     }
     
-    function getBalance(address _address) public onlyOwner returns (uint256) {        
+    function getBalance(address _address) public returns (uint256) {        
         return token.balanceOf(_address);
     }
-
-    function setMinimumReward(uint32 _minimumReward) public onlyOwner {
-        minimumReward = _minimumReward;
-    }
-
-    function setMaximumReward(uint32 _maximumReward) public onlyOwner {
-        maximumReward = _maximumReward;
-    }
-
-    function setBalanceToCompany(address _companyAddress, uint256 _balance) payable onlyOwner {        
-        token.mint(_companyAddress,_balance);
-        tokensMinted.add(_balance);
+    
+    function registerCompany(string _companyName, string _email) public 
+    {
+         if(companyMap[msg.sender].timestamp != 0)  revert();
+         
+        company memory newCompany;      
+        newCompany.name = _companyName;
+        newCompany.email = _email;
+        newCompany.timestamp = block.timestamp;
+        newCompany.status = Status.Registered;    
+        
+        companyMap[msg.sender] = newCompany; 
+        companies.push(newCompany);        
     }
     
-    function insertCompany(address _companyAddress, uint256 _balance) public onlyOwner returns (uint256) {    
-        if(companyMap[_companyAddress].companyAddress != address(0))  revert();
-        require(tokensMinted.add(_balance) <= MAXIMUM_SUPPLY);
+    
+    function loadBalanceToCompany() public payable returns (uint256) {    
+       
+        if(companyMap[msg.sender].timestamp == 0)  revert();
+        require(tokensMinted.add(msg.value) <= MAXIMUM_SUPPLY);
+        if(msg.value < 1 ether) throw; // accept exactly 1 ether and nothing else
+      
+        LYAccount.transfer(msg.value);
         
-        company memory newCompany;    
-        newCompany.companyAddress = _companyAddress;    
-        newCompany.timestamp = block.timestamp;
-        newCompany.status = Status.Active;    
-        companyMap[_companyAddress] = newCompany; 
-
-        companies.push(newCompany);        
-        setBalanceToCompany(_companyAddress,_balance);
+        token.mint(msg.sender, msg.value);
+        tokensMinted.add(msg.value);
         
-        return  token.balanceOf(_companyAddress);
+        company storage cmp = companyMap[msg.sender];    
+        cmp.status = Status.Payed;
+        
+        companyMap[msg.sender] = cmp; 
+        companies.push(cmp);     
+        
+        return  token.balanceOf(msg.sender);
         
     }
     
     //company adds product
-    function insertProduct(uint32 _productId, uint32 _duration, uint32 _promotionCount) public returns (uint32) {                      
+    function addProduct (uint32 _productId, uint32 _duration, uint32 _promotionCount) public returns (uint32) {                      
         if (productMap[_productId].productId == _productId) revert();                
-        if (companyMap[msg.sender].companyAddress != msg.sender ) revert();
+        if (companyMap[msg.sender].timestamp == 0 ) revert();
 
         product memory newProduct;
         newProduct.productId = _productId;
@@ -125,7 +137,7 @@ contract Promotions is Ownable {
         newProduct.endDate = now + _duration * 1 seconds;
         newProduct.promotionCount = _promotionCount;
         newProduct.timestamp = block.timestamp;
-        newProduct.status = Status.Active;
+        newProduct.status = AvaliableStatus.Active;
         newProduct.companyAddress = msg.sender;       
 
         productMap[_productId] = newProduct;
@@ -135,24 +147,24 @@ contract Promotions is Ownable {
     }
 
     //company adds barcode
-    function insertBarcode(uint32 _productId, uint32 _barcodeNumber, uint32 _rewardAmount) payable returns(bool) {       
+    function addBarcode(uint32 _productId, uint32 _barcodeNumber, uint32 _rewardAmount) payable returns(bool) {       
          //product not exist or product company != companyId
         if (productMap[_productId].productId == 0 || productMap[_productId].companyAddress != msg.sender) revert(); 
         //barcode exist
-        if (barcodeMap[keccak256(_barcodeNumber)].barcodeId != 0) revert(); 
-        //amount control
-        if (_rewardAmount < minimumReward || _rewardAmount > maximumReward) revert();        
+        if (barcodeMap[keccak256(_barcodeNumber)].barcodeId != 0) revert();   
+        
+        if(token.balanceOf(msg.sender) < insertBarcodeWei) revert();
 
         barcode memory newBarcode;
         newBarcode.barcodeId = keccak256(_barcodeNumber);
         newBarcode.timestamp = block.timestamp;
         newBarcode.rewardAmountInWei = _rewardAmount;
-        newBarcode.status = Status.Active; // isUsed
+        newBarcode.status = AvaliableStatus.Active; // isUsed
         barcodeMap[newBarcode.barcodeId] = newBarcode;
 
         barcodes.push(newBarcode);        
         //bakiyesinde azaltma yap
-        token.transferFrom(msg.sender, owner, insertProductWei);
+        token.transferFrom(msg.sender, owner, insertBarcodeWei);
 
         return true;
     }
@@ -165,15 +177,15 @@ contract Promotions is Ownable {
         //campaing expired
         if(productMap[_productId].startDate > now || productMap[_productId].endDate < now ) revert();
 
-        barcode  storage barcode2= barcodeMap[keccak256(_barcodeNumber)];
-        if (barcode2.status == Status.Active) {
-            barcode2.status = Status.Passive;
-            barcodeMap[keccak256(_barcodeNumber)] = barcode2;
+        barcode storage luckyBarcode= barcodeMap[keccak256(_barcodeNumber)];
+        if (luckyBarcode.status == AvaliableStatus.Active) {
+            luckyBarcode.status = AvaliableStatus.Passive;
+            barcodeMap[keccak256(_barcodeNumber)] = luckyBarcode;
             
-            product storage product2  = productMap[_productId];
-            product2.promotionCount = product2.promotionCount-1;
+            product storage productUpdate  = productMap[_productId];
+            productUpdate.promotionCount = productUpdate.promotionCount-1;
             
-            token.transferFrom(product2.companyAddress, msg.sender, barcode2.rewardAmountInWei);
+            token.transferFrom(productUpdate.companyAddress, msg.sender, luckyBarcode.rewardAmountInWei);
             
             return 1;
         }        
